6f69f3a2cfa905ff89adc49c5a4f67cbe9ef8369
cn <406794631@qq.com>
2020-10-15 20:29:59 +0800

    列表页方法

--- app/Http/Controllers/Index/SearchController.php
+++ app/Http/Controllers/Index/SearchController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Index;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Model\Goods;
 class SearchController extends Controller
 {
     //
     function search(){
-        return view('index.search');
+        $pageSize=config('app.pageSize');
+        $data=Goods::paginate($pageSize);
+        return view('index.search',['data'=>$data]);
     }
     function seckill(){
+
         return view('index.seckill');
     }
 }
