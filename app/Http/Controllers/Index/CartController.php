ddb6b595de881ece446c67354aba55dc095a3695
cn <406794631@qq.com>
2020-10-16 22:58:43 +0800

    购物车添加验证

---
+++ app/Http/Controllers/Index/CartController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\Goods;
+class CartController extends Controller
+{
+    //
+    public function cartdo()
+    {
+        $user=session('login');
+        if(!$user){
+            // dd($_SERVER);
+            $url = $_SERVER['HTTP_REFERER'];
+            return json_encode(['code'=>1,'msg'=>'您还没登录','url'=>$url]);
+        }
+        $goods_id=request()->goods_id;
+        $buy_number=request()->buy_number;
+        $goods=Goods::find($goods_id);
+        if($goods->goods_number<$buy_number){
+            return json_encode(['code'=>2,'msg'=>'库存不足']);
+        }
+    }
+}
